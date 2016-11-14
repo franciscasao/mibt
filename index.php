6022c636c78cb760c02247e1072ad950799acd65
karlitu <fcasao@gbox.adnu.edu.ph>
2016-11-14 18:52:03 +0800

    Uploaded files for Bootstrap, FontAwesome and Logos

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<link rel="icon" href="img/favicon.ico">
+
+		<title>MIBT</title>
+
+		<!-- Bootstrap core CSS -->
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/font-awesome.min.css" rel="stylesheet">
+
+		<!-- Custom CSS -->
+		<link href="css/custom.css" rel="stylesheet">
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+
+	<style type="text/css">
+		body {
+			background-color: white;
+		}
+	</style>
+
+	<body>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 text-center">
+					<div class="logo-container">
+						<img src="img/no_word_logo.png" width="50%">
+						<h3>MCHL - Institute of Business and Technology</h3>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="form-group"></div>
+				</div>
+			</div>
+		</div>
+
+		<!-- Bootstrap core JavaScript
+		================================================== -->
+		<!-- Placed at the end of the document so the pages load faster -->
+		<script src="js/jquery-2.2.3.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
